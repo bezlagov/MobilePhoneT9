eb1c8e6f1c652ef024a84129db2e9dd5eb5dc6b2
djeagle <djeagle3@rambler.ru>
2016-06-03 15:48:39 +0300

    d

---
+++ MobilePhoneT9/Model/Data.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilePhoneT9.Model
+{
+    class Data
+    {
+        private static Data _instance;
+        public static Data Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new Data();
+                return _instance;
+            }
+        }
+        
+        public List<Dict> dictionaryData;
+        private Data()
+        {
+            dictionaryData = new List<Dict>();
+        }
+    }
+}

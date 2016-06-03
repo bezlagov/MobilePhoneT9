f20ca46fa1deaae245fb7aa833eb470adf05bdeb
djeagle <djeagle3@rambler.ru>
2016-06-03 15:48:34 +0300

    s

---
+++ MobilePhoneT9/Model/Dict.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilePhoneT9.Model
+{
+    class Dict
+    {
+        public int Id { get; set; }
+        public string Russian { get; set; }
+        public string English { get; set; }
+        public Dict(int id, string russian, string english)
+        {
+            Id = id;
+            Russian = russian;
+            English = english;
+        }
+    }
+}
